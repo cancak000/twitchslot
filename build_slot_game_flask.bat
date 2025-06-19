79af061f016374ba8c0dfe9388d4602b00020065
cancak <tosh1kazu.y0429@gmail.com>
2025-06-19 18:56:11 +0900

    release用準備

--- build_slot_game_flask.bat
+++ build_slot_game_flask.bat
@@ -8,7 +8,7 @@ pyinstaller ^
   --add-data "image;image" ^
   --add-data "sound;sound" ^
   --add-data "setting.env;." ^
-  slot_game_with_flask.py
+  src/slot_game_with_flask.py
 
 echo ✅ ビルド完了！distフォルダを確認してください。
 pause
